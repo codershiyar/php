81f41b0ec3b8e5e8e510d85b29a140643c37e44b
Shiyar Jamo <1azadjemo@gmail.com>
2020-02-09 12:11:10 +0100

    update files

---
+++ LESSON 17 - الدرس/index.php
@@ -0,0 +1,34 @@
+
+<?php
+
+// break;
+// continue;
+
+// $counter = 1;
+// while($counter <=10){
+//     // if($counter == 6 || $counter == 3){
+//     //     ++$counter;
+//     //     continue;
+//     // }
+//     if($counter == 5){
+//     break;
+//     }
+//     echo "<div>عدد مرات تكرار تنفيذ اوامر $counter</div>";
+//     ++$counter;
+// }
+
+
+
+for($counter = 1; $counter<=10; ++$counter){
+    if($counter == 5){
+    break;
+    }
+    echo "<div>عدد مرات تكرار تنفيذ اوامر $counter</div>";
+}
+
+?>
+
+
+
+
+
